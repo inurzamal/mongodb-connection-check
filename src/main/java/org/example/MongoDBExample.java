caa535a769d1da94dc860d66a4b940198e53f5df
Nurzamal <n.zaman98@gmail.com>
2023-07-21 00:45:39 +0530

    maven quickstart project for mongodb connection check

---
+++ src/main/java/org/example/MongoDBExample.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import com.mongodb.client.*;
+import org.bson.Document;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MongoDBExample {
+    public static void main(String[] args) {
+        // MongoDB connection parameters
+        String host = "localhost";
+        int port = 27017; // Default MongoDB port
+        String databaseName = "mydb";
+        String collectionName = "mycollection";
+
+        // Set up the MongoDB client
+        MongoClient mongoClient = MongoClients.create("mongodb://" + host + ":" + port);
+
+        try {
+            // Get the database
+            MongoDatabase database = mongoClient.getDatabase(databaseName);
+
+            // Get the collection
+            MongoCollection<Document> collection = database.getCollection(collectionName);
+
+            // Insert a document (optional, you can skip this)
+//            Document document = new Document("name", "John Doe").append("age", 30);
+//            collection.insertOne(document);
+
+            // Query and print all documents in the collection
+            FindIterable<Document> documents = collection.find();
+            for (Document doc : documents) {
+                System.out.println(doc.toJson());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Close the client when done
+            mongoClient.close();
+        }
+    }
+}
+
